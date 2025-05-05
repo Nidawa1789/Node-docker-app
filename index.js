07cb0c4d9b6cd06487997b1c69888678c47fd977
Madani <105324725+Nidawa1789@users.noreply.github.com>
2025-05-05 12:51:55 +0000

    Initial commit: Node Docker app

---
+++ index.js
@@ -0,0 +1,16 @@
+const http = require('http');
+const PORT = process.env.PORT || 3000;
+
+const server = http.createServer((req, res) => {
+  if (req.url === '/status') {
+    res.writeHead(200);
+    res.end('OK');
+  } else {
+    res.writeHead(200);
+    res.end('Hello from Node.js Docker app!');
+  } 
+});
+
+server.listen(PORT, () => {
+  console.log(`Server listening on port ${PORT}`);
+})
